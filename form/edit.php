586f580c3d79d9261263ce96ec5758ab8baf21a9
Muhammad Sumon Molla Selim <smseleem@gmail.com>
2019-03-09 12:56:33 +0600

    edit page title changed

--- form/edit.php
+++ form/edit.php
@@ -60,7 +60,7 @@ $user = $stmt->fetch();
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>PHP User Form</title>
+    <title>PHP User Update</title>
     <link rel="stylesheet" href="//stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
           integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <link rel="stylesheet" href="//getbootstrap.com/docs/4.3/examples/sign-in/signin.css">
