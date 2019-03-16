75a6076e242dc488dd04600cf2193cdde0c3b732
Muhammad Sumon Molla Selim <smseleem@gmail.com>
2019-03-16 12:52:59 +0600

    csrf example added

--- index.php
+++ index.php
@@ -1,5 +1,23 @@
-<?php
-$place = 'Dhaka';
-$first_variable = '<h2>I am in '.$place.'</h2>';
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
 
-echo $first_variable;
+<p>
+    <?php echo htmlspecialchars($_POST['message']) ?? ''; ?>
+</p>
+
+
+<form action="" method="post">
+    <input type="text" name="message">
+    <button type="submit">Send</button>
+</form>
+
+</body>
+</html>
